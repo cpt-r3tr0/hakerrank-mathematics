3df1e1077df8cc2edc9b6e7a9f2efd421634f29d
Rupesh Boridya <rbordiya22@hotmail.com>
2018-12-02 11:30:56 +0530

    new problem

---
+++ Number Theory/LucyAndFlowers.py
@@ -0,0 +1,98 @@
+#!/bin/python3
+
+import os
+import sys
+import collections
+import itertools
+
+M = 1000000009
+mod_fact = []
+mod_inv = []
+mod_fact_inv = []
+nCr_arr = []
+
+def memodict(f):
+    """ Memoization decorator for a function taking a single argument """
+    # http://code.activestate.com/recipes/578231-probably-the-fastest-memoization-decorator-in-the-/
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+    return memodict().__getitem__
+
+def modularInverse( n) :
+# Moulo inverse 1 to n
+# https://www.geeksforgeeks.org/modular-multiplicative-inverse-1-n/
+
+    dp =[0]*(n+1)
+    dp[0] = dp[1] = 1
+    for i in range( 2, n+1) :
+        dp[i] = dp[M % i] *(M - M // i) % M
+
+    return dp
+
+def precalc_fact(n):
+    arr = [0]*(n+1)
+    arr[0], arr[1] = 1, 1
+    for i in range(2, len(arr)):
+        arr[i] = (i * arr[i-1]) % M
+
+    return arr
+
+def precalc_factinv(n):
+    arr = [0]*(n+1)
+    arr[0], arr[1] = 1, 1
+    for i in range(2, len(arr)):
+        arr[i] = (mod_inv[i] * arr[i-1]) % M
+
+    return arr
+
+def catalan(n):
+    # Table to store results of subproblems
+    arr = [0 for i in range(n + 1)]
+
+    # Initialize first two values in table
+    arr[0] = 1
+    if n > 0:
+        arr[1] = 1
+
+    if (n == 0 or n == 1):
+        return arr
+
+    # Fill entries in catalan[] using recursive formula
+    for i in range(1, n):
+        arr[i+1] = 2*(2*i + 1)*arr[i] % M
+        arr[i+1] = arr[i+1]*mod_inv[i+2] % M
+
+    return arr
+
+def nCr(n, r):
+    x = mod_fact[n]*mod_fact_inv[n-r] % M
+    return x*mod_fact_inv[r] % M
+
+@memodict
+def solve(n):
+    return sum(nCr(n,k)*mod_catalan[k] for k in range(1, n+1)) % M
+
+if __name__ == '__main__':
+    t = int(input())
+
+    n_arr = []
+    for t_itr in range(t):
+        n = int(input())
+        n_arr.append(n)
+
+    n_max = max(n_arr) #
+    mod_fact = precalc_fact(n_max)
+    mod_inv = modularInverse(n_max+2)
+    mod_fact_inv = precalc_factinv(n_max)
+    mod_catalan = catalan(n_max)
+
+
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    for n in n_arr:
+        result = solve(n)
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
