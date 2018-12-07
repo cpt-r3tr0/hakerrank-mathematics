96df9bf9f9f6de7b444954db5c463e7c8641efc6
Rupesh Boridya <rbordiya22@hotmail.com>
2018-12-07 17:13:13 +0530

    new Problem

---
+++ Fundamentals/MaximumDraws.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n ; i++){
+            int p = sc.nextInt();
+            System.out.println(p+1);
+
+        }
+    }
+}
