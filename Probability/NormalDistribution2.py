717852619f24b4ba1af94ecacdb033406cab36be
Rupesh Boridya <rbordiya22@hotmail.com>
2019-01-15 15:29:56 +0530

    new solution

---
+++ Probability/NormalDistribution2.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from math import *
+def phi(x, mu=20, sigma=2):
+    return (1.0 + erf((x-mu) / (sigma * sqrt(2.0)))) / 2.0
+
+print '%.3f' % phi(19.5)
+print '%.3f' % ( phi(20) + phi(22) - 1)
